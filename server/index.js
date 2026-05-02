3d0ba649c48b6d78eb74c95bb3dd0026a6152b54
Ahmed9253 <ahmedjutt9253@gmail.com>
2026-05-02 19:42:41 +0500

    commit and push server initialised

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
