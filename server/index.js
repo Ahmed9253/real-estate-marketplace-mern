3b55e98d76f12410b5a9cd0cca73df5933445376
Ahmed9253 <ahmedjutt9253@gmail.com>
2026-05-02 22:03:36 +0500

    backend initialised

--- server/index.js
+++ server/index.js
@@ -4,15 +4,20 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
+// ========== Initialize Express app ==========
 const app = express();
-app.use(cors());
 
+// ========== Middleware ==========
+app.use(cors());
+app.use(express.json());
+
+// ========== Routes ==========
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// ========== Connect to MongoDB and start the server ==========
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
