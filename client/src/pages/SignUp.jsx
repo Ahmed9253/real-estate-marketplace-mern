b7c78a9b49f91f4c7ffaaa4e965d0f0b119ed210
Ahmed9253 <ahmedjutt9253@gmail.com>
2026-05-06 01:28:04 +0500

    compelete sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { Link, Links } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>SignOut</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button className="bg-blue-950 p-3 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex justify-center gap-2 mt-5">
+        <p>Already have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700 hover:text-blue-950">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
-
 export default SignUp;
