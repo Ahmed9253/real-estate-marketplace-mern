cc45649bf621df29863059bedfb78d4d6aee1f70
Ahmed9253 <ahmedjutt9253@gmail.com>
2026-05-09 20:52:11 +0500

    redux toolkit , slicer and users are added.....fixed....

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -9,21 +9,28 @@ const initialState = {
 const userSlice = createSlice({
   name: "user",
   initialState,
+
   reducers: {
     signInStart(state) {
       state.loading = true;
+      state.error = null;
     },
+
     signInSuccess(state, action) {
       state.currentUser = action.payload;
       state.loading = false;
       state.error = null;
     },
+
     signInFailure(state, action) {
-      state.error = action.payload;
       state.loading = false;
+      state.error = action.payload;
     },
+
     signOut(state) {
       state.currentUser = null;
+      state.loading = false;
+      state.error = null;
     },
   },
 });
